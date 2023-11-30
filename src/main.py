330aa2d13181511914e3dfe0f4bd847309273d12
Zhyldyz <jyldyz0610@gmail.com>
2023-11-30 12:51:26 +0100

    To_DO app

--- src/main.py
+++ src/main.py
@@ -63,4 +63,4 @@ async def delete_todo(todo_id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
